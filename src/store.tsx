ec712bfc4ded096aae541fef7000b13546cffd1b
Pașca Ionuț <pascaionut94@gmail.com>
2022-01-17 12:35:06 +0200

    dsadsa

--- src/store.tsx
+++ src/store.tsx
@@ -11,6 +11,10 @@ interface Props {
 
 const AppStateContext = createContext<Props | null>(null);
 
+// Change from branch A
+// Change from branch B
+// Change from branch C
+// Change from branch D
 export const AppStateProvider: React.FC<Props> = ({ children }) => {
   const [state, setState] = useState<State>({
     title: "teleport-project-react"
